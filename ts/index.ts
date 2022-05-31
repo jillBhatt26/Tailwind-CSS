d3305885449458764debbbd9fde5dec7f269d1e0
jill bhatt <bhattjill@gmail.com>
2022-05-31 11:38:14 +0530

    Converted js to ts

--- ts/index.ts
+++ ts/index.ts
@@ -1,11 +1,10 @@
-"use strict";
-const menu = document.querySelector('#menu');
-const burger = document.querySelector('#burger');
+const menu = document.querySelector('#menu') as HTMLDivElement;
+const burger = document.querySelector('#burger') as HTMLUListElement;
+
 burger.addEventListener('click', () => {
     if (menu.classList.contains('hidden')) {
         menu.classList.remove('hidden');
-    }
-    else {
+    } else {
         menu.classList.add('hidden');
     }
 });
